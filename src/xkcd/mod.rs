a4fef3e1335f5fe138ae1e267527310d6cbc089b
HexPandaa <47880094+HexPandaa@users.noreply.github.com>
2021-09-30 17:28:20 +0200

    WIP: Implement API fetching and parsing

--- src/xkcd/mod.rs
+++ src/xkcd/mod.rs
@@ -1,5 +1,26 @@
 use chrono::prelude::*;
 
+// type Result<T> = std::result::Result<T, XkcdError>;
+
+#[derive(Debug, Clone)]
+enum XkcdError {
+    DownloadError,
+    JsonError,
+}
+
+impl std::fmt::Display for XkcdError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            XkcdError::DownloadError => {
+                write!(f, "Error downloading comic, maybe the id is invalid?")
+            }
+            XkcdError::JsonError => {
+                write!(f, "Error decoding JSON response")
+            }
+        }
+    }
+}
+
 /// The representation of a comic
 struct Comic {
     title: String,
